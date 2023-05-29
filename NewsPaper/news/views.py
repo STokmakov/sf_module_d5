950852795ad68f4c004fc62c84686bb2cb60a5ac
Сергей Токмаков <s.tokmakov@ermolinskie.com>
2023-05-29 15:57:32 +0300

    new

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,87 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from datetime import datetime
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from .models import Post
+from .filters import PostFilter
+from .forms import PostForm
+
+
+class SearchPostList(ListView):
+    model = Post
+    ordering = 'dateCreation'
+    template_name = 'search.html'
+    context_object_name = 'newsposts'
+    paginate_by = 10                          # количество записей на странице
+
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = PostFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset
+        return context
+
+class PostList(ListView):
+    model = Post
+    ordering = 'dateCreation'
+    template_name = 'posts.html'
+    context_object_name = 'newsposts'
+    paginate_by = 10                          # количество записей на странице
+
+
+    def get_queryset(self):
+        return Post.objects.filter(categoryType='NW')
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'newspost'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = None
+        return context
+
+    def get_queryset(self):
+
+        return Post.objects.filter(categoryType='NW')
+
+
+class PostCreate(LoginRequiredMixin, CreateView):
+  #  permission_required = ('news.add_post',)
+    raise_exception = True
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+   # permission_required = ('news.change_post',)
+    raise_exception = True
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+# Представление удаляющее товар.
+class PostDelete(LoginRequiredMixin, DeleteView):
+   # permission_required = ('news.delete_post',)
+    raise_exception = True
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
+
